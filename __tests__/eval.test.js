e3601655a25cdc9d47fcaf6ab1fe2143f50f6cb7
Michael Lundin <mikel@ifit.com>
2018-05-04 08:21:10 -0700

    Convert tests to tape

--- __tests__/eval.test.js
+++ __tests__/eval.test.js
@@ -1,6 +1,26 @@
+const test = require('tape')
+const sinon = require('sinon')
 const evalInContext = require('../eval')
 
-describe('evalInContext', () => {
+test('evalInContext - evals global variable', (t) => {
+  global.property = 'property'
+  t.equal(evalInContext('property', {}), 'property')
+  t.end()
+})
+
+test('evalInContext - evals function in context', (t) => {
+  const clientid = sinon.stub().returns('test')
+  t.equal(evalInContext('clientid()', { clientid }), 'test')
+  t.end()
+})
+
+test('throws error if variable does not exist', (t) => {
+  t.throws((() => evalInContext('notHere', {})))
+  t.end()
+})
+
+
+/* describe('evalInContext', () => {
   it('evals global variable', () => {
     global.property = 'property'
     expect(evalInContext('property', {})).toBe('property')
@@ -14,4 +34,4 @@ describe('evalInContext', () => {
   it('throws error if variable does not exist', () => {
     expect(() => evalInContext('notHere', {})).toThrowError()
   })
-})
+}) */
--- __tests__/sql.test.js
+++ __tests__/sql.test.js
@@ -1,92 +1,120 @@
+const test = require('tape')
+const sinon = require('sinon')
 const { parseSelect, applySelect } = require('../sql.js')
 
-describe('parseSelect', () => {
-  it('parses simple SQL correctly', () => {
-    const subject = "SELECT * FROM 'topic'"
-    const results = parseSelect(subject)
-    expect(results.select).toEqual([{ field: '*', alias: undefined }])
-    expect(results.topic).toBe('topic')
-    expect(results.where).toBe(undefined)
-  })
-
-  it('parses lowercase simple SQL correctly', () => {
-    const subject = "select * from 'topic'"
-    const results = parseSelect(subject)
-    expect(results.select).toEqual([{ field: '*', alias: undefined }])
-    expect(results.topic).toBe('topic')
-    expect(results.where).toBe(undefined)
-  })
-
-  it('parses where clause correctly', () => {
-    const subject = "SELECT * FROM 'topic' WHERE name='Bob'"
-    const results = parseSelect(subject)
-    expect(results.select).toEqual([{ field: '*', alias: undefined }])
-    expect(results.topic).toBe('topic')
-    expect(results.where).toBe("name='Bob'")
-  })
-
-  it('parses multiple SELECT properties correctly', () => {
-    const subject = "SELECT name, age, maleOrFemale AS gender FROM 'topic'"
-    const results = parseSelect(subject)
-    expect(results.select).toEqual([
-      { field: 'name', alias: undefined},
-      { field: 'age', alias: undefined },
-      { field: 'maleOrFemale', alias: 'gender'}
-    ])
-  })
+test('parseSelect - parses simple SQL correctly', (t) => {
+  const subject = "SELECT * FROM 'topic'"
+  const results = parseSelect(subject)
+  t.deepEqual(results.select, [{ field: '*', alias: undefined }])
+  t.equal(results.topic, 'topic')
+  t.equal(results.where, undefined)
+  t.end()
 })
 
-describe('applySelect', () => {
-  describe('Simple select with buffered string handled correctly', () => {
-    const select = [{ field: '*', alias: undefined }]
-    const payload = Buffer.from(JSON.stringify({name: 'Bob'}), 'utf8')
-    const context = {}
-    const event = applySelect({ select, payload, context })
-    expect(event).toEqual({ name: 'Bob' })
-  })
+test('parseSelect - parses lowercase simple SQL correctly', (t) => {
+  const subject = "select * from 'topic'"
+  const results = parseSelect(subject)
+  t.deepEqual(results.select, [{ field: '*', alias: undefined }])
+  t.equal(results.topic, 'topic')
+  t.equal(results.where, undefined)
+  t.end()
+})
 
-  describe('Simple select with non-JSON handled correctly', () => {
-    const select = [{ field: '*', alias: undefined }]
-    const payload = 'Bob'
-    const context = {}
-    const event = applySelect({ select, payload, context })
-    expect(event).toEqual( 'Bob' )
-  })
+test('parseSelect - parses where clause correctly', (t) => {
+  const subject = "SELECT * FROM 'topic' WHERE name='Bob'"
+  const results = parseSelect(subject)
+  t.deepEqual(results.select, [{ field: '*', alias: undefined }])
+  t.equal(results.topic, 'topic')
+  t.equal(results.where, "name='Bob'")
+  t.end()
+})
 
-  describe('Aliased wildcard with non-JSON handled correctly', () => {
-    const select = [{ field: '*', alias: 'name' }]
-    const payload = 'Bob'
-    const context = {}
-    const event = applySelect({ select, payload, context })
-    expect(event).toEqual({ 'name': 'Bob' })
-  })
 
-  describe('Unaliased wildcard plus function results in flattened output', () => {
-    const select = [
-      { field: '*', alias: undefined },
-      { field: 'clientid()', alias: undefined }
-    ]
-    const clientIdFunc = jest.fn()
-    const payload = Buffer.from(JSON.stringify({name: 'Bob'}), 'utf8')
-    const context = { clientid: clientIdFunc }
-    const event = applySelect({ select, payload, context })
-    expect(clientIdFunc).toHaveBeenCalledTimes(1)
-    expect(event).toEqual({ name: 'Bob' })
-  })
+test('parseSelect - parses multiple SELECT properties correctly', (t) => {
+  const subject = "SELECT name, age, maleOrFemale AS gender FROM 'topic'"
+  const results = parseSelect(subject)
+  t.deepEqual(results.select, [
+    { field: 'name', alias: undefined},
+    { field: 'age', alias: undefined },
+    { field: 'maleOrFemale', alias: 'gender'}
+  ])
+  t.end()
+})
 
-  describe('Aliased wildcard plus function results in nested output', () => {
-    const select = [
-      { field: '*', alias: 'message' },
-      { field: 'clientid()', alias: undefined }
-    ]
-    const clientIdFunc = jest.fn()
-    const payload = Buffer.from(JSON.stringify({name: 'Bob'}), 'utf8')
-    const context = { clientid: clientIdFunc }
-    const event = applySelect({ select, payload, context })
-    expect(clientIdFunc).toHaveBeenCalledTimes(1)
-    expect(event).toEqual({ message: { name: 'Bob' } })
-  })
+test('applySelect - Simple select with buffered string handled correctly', (t) => {
+  const select = [{ field: '*', alias: undefined }]
+  const payload = Buffer.from(JSON.stringify({name: 'Bob'}), 'utf8')
+  const context = {}
+  const event = applySelect({ select, payload, context })
+  t.deepEqual(event, { name: 'Bob' })
+  t.end()
+})
 
+test('applySelect - Simple select with non-JSON handled correctly', (t) => {
+  const select = [{ field: '*', alias: undefined }]
+  const payload = 'Bob'
+  const context = {}
+  const event = applySelect({ select, payload, context })
+  t.equal(event, 'Bob')
+  t.end()
+})
+
+test('applySelect - Aliased wildcard with non-JSON handled correctly', (t) => {
+  const select = [{ field: '*', alias: 'name' }]
+  const payload = 'Bob'
+  const context = {}
+  const event = applySelect({ select, payload, context })
+  t.deepEqual(event, { 'name': 'Bob'})
+  t.end()
+})
+
+test('applySelect - Unaliased wildcard plus function results in flattened output', (t) => {
+  const select = [
+    { field: '*', alias: undefined },
+    { field: 'clientid()', alias: undefined }
+  ]
+  const clientIdFunc = sinon.stub().returns(undefined);
+  const payload = Buffer.from(JSON.stringify({name: 'Bob'}), 'utf8')
+  const context = { clientid: clientIdFunc }
+  const event = applySelect({ select, payload, context })
+  t.ok(clientIdFunc.calledOnce)
+  t.deepEqual(event, { name: 'Bob', 'clientid()': undefined })
+  t.end()
+})
+
+test('applySelect - Aliased wildcard plus function results in nested output', (t) => {
+  const select = [
+    { field: '*', alias: 'message' },
+    { field: 'clientid()', alias: undefined }
+  ]
+  const clientIdFunc = sinon.stub().returns(undefined);
+  const payload = Buffer.from(JSON.stringify({name: 'Bob'}), 'utf8')
+  const context = { clientid: clientIdFunc }
+  const event = applySelect({ select, payload, context })
+  t.ok(clientIdFunc.calledOnce)
+  t.deepEqual(event, { message: { name: 'Bob' }, 'clientid()': undefined })
+  t.end()
+})
+
+test('applySelect - Function results are appeneded to output', (t) => {
+  const select = [
+    { field: '*', alias: 'message' },
+    { field: 'clientid()', alias: 'theClientId' }
+  ]
+  const clientIdFunc = sinon.stub().returns('12345')
+  const payload = Buffer.from(JSON.stringify({name: 'Bob'}), 'utf8')
+  const context = { clientid: clientIdFunc }
+  const event = applySelect({ select, payload, context })
+  t.ok(clientIdFunc.calledOnce)
+  t.deepEqual(event, { message: { name: 'Bob' }, 'theClientId': '12345' })
+  t.end()
+})
+
+
+
+
+
+/*
   describe('Function results are appeneded to output', () => {
     const select = [
       { field: '*', alias: 'message' },
@@ -101,3 +129,4 @@ describe('applySelect', () => {
   })
 
 })
+*/
