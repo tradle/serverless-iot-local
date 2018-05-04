9c0823663119f74ce3b4970b9bbae4230348b2c6
Mike Lundin <MikeTheReader@users.noreply.github.com>
2018-05-04 09:47:34 -0700

    Convert tests to tape (#6)
    * Convert tests to tape, add coverage with istanbul

--- __tests__/eval.test.js
+++ __tests__/eval.test.js
@@ -1,17 +1,20 @@
+const test = require('tape')
+const sinon = require('sinon')
 const evalInContext = require('../eval')
 
-describe('evalInContext', () => {
-  it('evals global variable', () => {
-    global.property = 'property'
-    expect(evalInContext('property', {})).toBe('property')
-  })
+test('evalInContext - evals global variable', (t) => {
+  global.property = 'property'
+  t.equal(evalInContext('property', {}), 'property')
+  t.end()
+})
 
-  it('evals function in context', () => {
-    const clientid = jest.fn().mockReturnValue('test')
-    expect(evalInContext('clientid()', { clientid })).toBe('test')
-  })
+test('evalInContext - evals function in context', (t) => {
+  const clientid = sinon.stub().returns('test')
+  t.equal(evalInContext('clientid()', { clientid }), 'test')
+  t.end()
+})
 
-  it('throws error if variable does not exist', () => {
-    expect(() => evalInContext('notHere', {})).toThrowError()
-  })
+test('throws error if variable does not exist', (t) => {
+  t.throws((() => evalInContext('notHere', {})))
+  t.end()
 })
